85cba4ccd7d55badc1427530897f7ce67752399e
irfnse <irfan.setiawan@solusi247.com>
2021-06-04 23:37:37 +0700

    add server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-
+// untuk menjalankan /dist
 app.use(serveStatic(__dirname + "/dist"));
+// menagtur port
 var port = process.env.PORT || 5000;
 app.listen(port);
